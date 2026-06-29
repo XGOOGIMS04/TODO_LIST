98993f4a642c9b52e8c89c5db8d900a187f933f7
XGOOGIMS04 <heeju2021.front.end@gmail.com>
2026-06-29 18:14:52 +0900

    feat: 동기부여 입력창 구현

--- main.js
+++ main.js
@@ -12,9 +12,12 @@ today = (`${year}.${month+1}.${day}`); /* 문자열 포맷팅 */
 date_section.innerHTML = today;
 
 
-// ----- 동기부여
+// ----- article_동기부여
 let text = document.querySelector("#text_box");
-text.addEventListener("keydown", down => { // 엔터 누르면 콘솔에 찍힘
-    if (down.key == 'Enter'){
-    console.log(text.value);
+text.addEventListener("keydown", down => { // 엔터 누르면 커서 사라짐(포커스 제거)
+    if (down.key == 'Enter' && !down.isComposing){ // 엔터 && 한글조합 끝나면 실행
+    text.blur(); // 커서 사라짐(포커스 제거)
+    localStorage.setItem("user_text", text.value); // setIte()함수로 localStorage에 아이템 추가
 }});
+const user_text = localStorage.getItem("user_text") || ""; // getItem() 함수로 localStorage의 아이템 읽기
+text.value = user_text; // input 창에 값 저장
